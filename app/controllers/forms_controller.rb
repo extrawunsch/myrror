f825276e22e8784de7bf1185a3b664999117b34c
Ramona Maltan <ramonamaltan@gmail.com>
2020-11-24 17:40:08 +0100

    added grid and css

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -8,16 +8,18 @@ class FormsController < ApplicationController
     @form = Form.new
     authorize @form
     @question = Question.new
+    @questions = Question.all
   end
 
   def create
+    @questions = Question.all
     @form = Form.new(form_params)
     @form.user = current_user
     # placeholder for proper presentation key
     @form.presentation_key = rand()
     authorize @form
     if @form.save
-      # need to connect question to form
+      # need to connect question with form_question if needed
       @question = Question.new(question_params)
       if @question.save
         redirect_to form_path(@form)
