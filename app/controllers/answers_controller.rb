908e77be298c9e4a4e057abf622be6c285cc0ae4
Christoph Bohne christoph-bohne@gmx.de <christoph-bohne@gmx.de>
2020-11-25 08:49:29 +0100

    Merged Master

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,5 +2,6 @@ class AnswersController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :new, :create ]
   def new
     @answer = Answer.new
+    @form = Form.find(params[:id])
   end
 end
