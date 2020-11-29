00e759dbebb3966d90f4f828fd0df62e9d3bd501
Niklas Koch <koch.niklas@yahoo.de>
2020-11-29 19:34:43 +0100

    changed logo and background color

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,7 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = false
 
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
@@ -60,4 +61,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #added manually by niklas
+  config.assets.check_precompiled_asset = false
 end
