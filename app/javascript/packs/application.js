4f2fe7875ff8ec853d89e33561425a2068ee163f
Ramona Maltan <ramonamaltan@gmail.com>
2020-11-29 15:13:48 +0100

    added search and select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,10 +29,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 
 import { initFlatpickr } from '../plugins/init_flatpickr';
+import { initSelect2 } from '../plugins/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   initFlatpickr();
+  initSelect2();
 });
 
 import "controllers"
