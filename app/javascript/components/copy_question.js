5470e706d42c706d23908a5382d91e6a5da3e517
Ramona Maltan <ramonamaltan@gmail.com>
2020-11-29 17:16:30 +0100

    works now

--- app/javascript/components/copy_question.js
+++ app/javascript/components/copy_question.js
@@ -1,12 +1,20 @@
-var firstDivContent = document.getElementById('mydiv1');
 const initCopyQuestion = () => {
-    const copyhere = document.getElementById('copyhere');
-    const pastehere = document.getElementById('question_question_content');
-    const button = document.querySelector('button')
-
-    button.addEventListener('click', () => {
-        pastehere.innerHTML = copyhere.innerHTML;
-      });
+  // element to copy
+  const copyhere = document.querySelectorAll('#copyhere');
+  // element where to paste
+  const pastehere = document.getElementById('question_question_content');
+  // Get an array of buttons from the page
+  const buttons = document.querySelectorAll("button");
+  // Loop through the resulting array
+  for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", function() {
+      // with i you can find the right element to copy
+      const elementtocopy = copyhere[i];
+      // paste it into other element
+      pastehere.innerHTML = elementtocopy.innerHTML;
+    });
   }
+
+}
   
-  export { initCopyQuestion };
+export { initCopyQuestion };
