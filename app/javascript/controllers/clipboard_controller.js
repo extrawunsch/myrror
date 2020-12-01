6251cfa3b65262c37e7e03ae65701b991baf3bf9
Agnès de Briey <debrieyagnes@gmail.com>
2020-12-01 15:53:53 +0100

    success page work on clipboard

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -3,7 +3,8 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "source" ]
   copy() {
-    this.sourceTarget.select()
+    
+    console.log(this.sourceTarget.select())
     document.execCommand("copy")
   }
 }
